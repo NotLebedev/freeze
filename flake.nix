5214509bd61df7221fd7e67be359c88932e33573
NotLebedev <artem-itf@yandex.ru>
2023-12-14 21:52:18 +0300

    Switch to main nuenv repo

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nuenv.url = "github:NotLebedev/nuenv/nu-87";
+    nuenv.url = "github:DeterminateSystems/nuenv";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
