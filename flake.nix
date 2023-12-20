ca9f8d878bac6ebb64515f11328e48e0472acdb1
NotLebedev <artem-itf@yandex.ru>
2023-12-20 17:47:17 +0300

    Update to nushell 88.1
    Change nuenv to follow nixpkgs of flake and update nushell to latest
    version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,10 @@
   description = "";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nuenv.url = "github:DeterminateSystems/nuenv";
+    nuenv = {
+      url = "github:DeterminateSystems/nuenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
