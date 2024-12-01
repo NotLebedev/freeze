3153ef46f020f87f82b346021e39c770a8714dc4
NotLebedev <artem-itf@yandex.ru>
2024-12-01 12:32:47 +0300

    Add test for packages

---
+++ checks/nu_scripts/default.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+let
+  all = pkgs.nushell-freeze.packages.nu_scripts;
+  just-temp = pkgs.nushell-freeze.packages.from_nu_scripts "temp" "sourced/temp.nu";
+in
+pkgs.nuenv.mkDerivation {
+  name = "checkPipeSyntax";
+  src = ./.;
+
+  build = ''
+    #!/usr/bin/env nu
+    use std assert
+
+    use ${all}/lib/nushell/nu_scripts/sourced/temp.nu c-to-k
+    assert equal (c-to-k 100 -r 0) '100 °C is 373 °K'
+
+    use ${just-temp}/lib/nushell/temp k-to-c
+    assert equal (k-to-c 373 -r 0) '373 °K is 100 °C'
+
+    mkdir $env.out
+  '';
+}
