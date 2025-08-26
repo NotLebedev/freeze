d1a44e8fc5404d0253105193798f27c9b37d1333
NotLebedev <notlebedev@gmail.com>
2025-08-26 13:51:57 +0300

    Update nu and rust dependencies to 0.106.1

--- checks/nu_scripts/default.nix
+++ checks/nu_scripts/default.nix
@@ -9,10 +9,13 @@ in
   use std assert
 
   use ${all}/lib/nushell/nu_scripts/sourced/temp.nu c-to-k
-  assert equal (c-to-k 100 -r 0) '100 °C is 373 °K'
+  assert equal (c-to-k 100 -r 0) '100 °C is 373.0 °K'
+
+  use ${all}/lib/nushell/nu_scripts/sourced/temp.nu c-to-k
+  assert equal (c-to-k 100 -r 0) '100 °C is 373.0 °K'
 
   use ${just-temp}/lib/nushell/temp k-to-c
-  assert equal (k-to-c 373 -r 0) '373 °K is 100 °C'
+  assert equal (k-to-c 373 -r 0) '373 °K is 100.0 °C'
 
   mkdir $env.out
 ''
